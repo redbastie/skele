82541315db2ef5c63cadd099f113a340594e3739
redbastie <redbastie@icloud.com>
2021-01-31 15:12:49 -0500

    custom user crud

--- src/Commands/CrudCommand.php
+++ src/Commands/CrudCommand.php
@@ -15,30 +15,38 @@ class CrudCommand extends Command
 
     public function handle()
     {
-        $modelParser = new ComponentParser('App\\Models', resource_path('views'), $this->argument('class'));
-        $modelTitles = Str::plural(preg_replace('/(.)(?=[A-Z])/u', '$1 ', $modelParser->className()));
-        $componentClass = Str::replaceLast($modelParser->className(), Str::studly($modelTitles), $this->argument('class'));
-        $componentParser = new ComponentParser('App\\Components', resource_path('views'), $componentClass);
+        if ($this->argument('class') == 'User') {
+            $this->createFiles('crud-user');
 
-        $this->createFiles('crud', [
-            'app/Components/DummyModels' => str_replace('.php', '', $componentParser->relativeClassPath()),
-            'resources/views/DummyViews' => str_replace('.blade.php', '', $componentParser->relativeViewPath()),
-            'DummyComponentNamespace' => $componentParser->classNamespace() . '\\' . $componentParser->className(),
-            'DummyModelNamespace' => $modelParser->classNamespace(),
-            'DummyModelVariables' => Str::camel($modelTitles),
-            'DummyModelVariable' => Str::camel(Str::singular($modelTitles)),
-            'DummyModelTitles' => $modelTitles,
-            'DummyModelTitle' => Str::singular($modelTitles),
-            'DummyModel' => $modelParser->className(),
-            'DummyRouteUri' => $dummyRouteUri = str_replace('.', '/', $componentParser->viewName()),
-            'DummyViewName' => $componentParser->viewName(),
-        ]);
+            $this->warn('<info>User</info> CRUD components & views generated! ' .
+                '<href=' . url('users') . '>' . url('users') . '</>');
+        }
+        else {
+            $modelParser = new ComponentParser('App\\Models', resource_path('views'), $this->argument('class'));
+            $modelTitles = Str::plural(preg_replace('/(.)(?=[A-Z])/u', '$1 ', $modelParser->className()));
+            $componentClass = Str::replaceLast($modelParser->className(), Str::studly($modelTitles), $this->argument('class'));
+            $componentParser = new ComponentParser('App\\Components', resource_path('views'), $componentClass);
 
-        $this->warn('<info>' . $this->argument('class') . '</info> CRUD components & views generated! ' .
-            '<href=' . url($dummyRouteUri) . '>' . url($dummyRouteUri) . '</>');
+            $this->createFiles('crud', [
+                'app/Components/DummyModels' => str_replace('.php', '', $componentParser->relativeClassPath()),
+                'resources/views/DummyViews' => str_replace('.blade.php', '', $componentParser->relativeViewPath()),
+                'DummyComponentNamespace' => $componentParser->classNamespace() . '\\' . $componentParser->className(),
+                'DummyModelNamespace' => $modelParser->classNamespace(),
+                'DummyModelVariables' => Str::camel($modelTitles),
+                'DummyModelVariable' => Str::camel(Str::singular($modelTitles)),
+                'DummyModelTitles' => $modelTitles,
+                'DummyModelTitle' => Str::singular($modelTitles),
+                'DummyModel' => $modelParser->className(),
+                'DummyRouteUri' => $dummyRouteUri = str_replace('.', '/', $componentParser->viewName()),
+                'DummyViewName' => $componentParser->viewName(),
+            ]);
 
-        if (!$this->fileExists($modelParser->relativeClassPath())) {
-            Artisan::call('skele:model ' . $this->argument('class'), [], $this->getOutput());
+            $this->warn('<info>' . $this->argument('class') . '</info> CRUD components & views generated! ' .
+                '<href=' . url($dummyRouteUri) . '>' . url($dummyRouteUri) . '</>');
+
+            if (!$this->fileExists($modelParser->relativeClassPath())) {
+                Artisan::call('skele:model ' . $this->argument('class'), [], $this->getOutput());
+            }
         }
     }
 }
